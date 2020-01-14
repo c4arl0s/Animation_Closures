17dd81bf65cc9978214879f0ed68b433bf5eb115
c4arl0s <c.santiago.cruz@gmail.com>
2020-01-14 11:29:42 -0600

    add playground

---
+++ AnimateWithDuration.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+import PlaygroundSupport
+
+let liveViewFrame = CGRect(x: 0, y: 0, width: 500, height: 500)
+let liveView = UIView(frame: liveViewFrame)
+liveView.backgroundColor = .white
+PlaygroundPage.current.liveView = liveView
+
+let smallFrame = CGRect(x: 0, y: 0, width: 100, height: 100)
+let square = UIView(frame: smallFrame)
+square.backgroundColor = .purple
+liveView.addSubview(square)
+
+UIView.animate(withDuration: 5.0, animations: {
+    square.backgroundColor = .orange
+    square.frame = CGRect(x: 150, y: 150, width: 200, height: 200)
+}) { (_) in
+    UIView.animate(withDuration: 5.0, animations: {
+        square.backgroundColor = .purple
+        square.frame = smallFrame
+    })
+}
